c971779dc15d380191e039ef0d040d3acfdfea48
pol <polcha.ch@gmail.com>
2018-08-26 11:16:30 +0700

    er

--- index.php
+++ index.php
@@ -221,9 +221,6 @@ else {
 			$arrPostData['messages'][0]['type'] = "text";
 			$arrPostData['messages'][0]['text'] = "คุณ " .$name. " คะ\nขณะนี้พนักงานไม่สามารถให้บริการท่านได้\nกรุณารอสักครู่ ท่านจะได้รับบริการโดยเร็วที่สุดค่ะ";
 			$arrPostData['messages'][0]['type'] = "sticker";
-			$arrPostData['messages'][0]['packageId'] = 1;
-			$arrPostData['messages'][0]['stickerId'] = 1;
-			$arrPostData['messages'][0]['type'] = "sticker";
 			$arrPostData['messages'][0]['packageId'] = 4;
 			$arrPostData['messages'][0]['stickerId'] = 238;
 		
