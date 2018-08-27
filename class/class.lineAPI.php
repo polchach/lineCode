f7da60691ef42c68ea3abc5b8fed57afc974958a
pol <polcha.ch@gmail.com>
2018-08-27 12:29:40 +0700

    ok

--- class/class.lineAPI.php
+++ class/class.lineAPI.php
@@ -334,5 +334,131 @@ class lineAPI {
 		
 		return $arrPostData['agents']['displayName'];
 	}
+	
+	// Rich Menu
+	
+function createNewRichmenu($channelAccessToken) {
+  $sh = <<< EOF
+  curl -X POST \
+  -H 'Authorization: Bearer $channelAccessToken' \
+  -H 'Content-Type:application/json' \
+  -d '{"size": {"width": 2500,"height": 1686},"selected": false,"name": "Controller","chatBarText": "Controller","areas": [{"bounds": {"x": 551,"y": 325,"width": 321,"height": 321},"action": {"type": "message","text": "up"}},{"bounds": {"x": 876,"y": 651,"width": 321,"height": 321},"action": {"type": "message","text": "right"}},{"bounds": {"x": 551,"y": 972,"width": 321,"height": 321},"action": {"type": "message","text": "down"}},{"bounds": {"x": 225,"y": 651,"width": 321,"height": 321},"action": {"type": "message","text": "left"}},{"bounds": {"x": 1433,"y": 657,"width": 367,"height": 367},"action": {"type": "message","text": "btn b"}},{"bounds": {"x": 1907,"y": 657,"width": 367,"height": 367},"action": {"type": "message","text": "btn a"}}]}' https://api.line.me/v2/bot/richmenu;
+EOF;
+  $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+  if(isset($result['richMenuId'])) {
+    return $result['richMenuId'];
+  }
+  else {
+    return $result['message'];
+  }
+}
+
+function getListOfRichmenu($channelAccessToken) {
+  $sh = <<< EOF
+  curl \
+  -H 'Authorization: Bearer $channelAccessToken' \
+  https://api.line.me/v2/bot/richmenu/list;
+EOF;
+  $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+  return $result;
+}
+
+function checkRichmenuOfUser($channelAccessToken, $userId) {
+  $sh = <<< EOF
+  curl \
+  -H 'Authorization: Bearer $channelAccessToken' \
+  https://api.line.me/v2/bot/user/$userId/richmenu
+EOF;
+  $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+  if(isset($result['richMenuId'])) {
+    return $result['richMenuId'];
+  }
+  else {
+    return $result['message'];
+  }
+}
+
+function unlinkFromUser($channelAccessToken, $userId) {
+  $sh = <<< EOF
+  curl -X DELETE \
+  -H 'Authorization: Bearer $channelAccessToken' \
+  https://api.line.me/v2/bot/user/$userId/richmenu
+EOF;
+  $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+  if(isset($result['message'])) {
+    return $result['message'];
+  }
+  else {
+    return 'success';
+  }
+}
+
+function deleteRichmenu($channelAccessToken, $richmenuId) {
+  if(!isRichmenuIdValid($richmenuId)) {
+    return 'invalid richmenu id';
+  }
+  $sh = <<< EOF
+  curl -X DELETE \
+  -H 'Authorization: Bearer $channelAccessToken' \
+  https://api.line.me/v2/bot/richmenu/$richmenuId
+EOF;
+  $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+  if(isset($result['message'])) {
+    return $result['message'];
+  }
+  else {
+    return 'success';
+  }
+}
+
+function linkToUser($channelAccessToken, $userId, $richmenuId) {
+  if(!isRichmenuIdValid($richmenuId)) {
+    return 'invalid richmenu id';
+  }
+  $sh = <<< EOF
+  curl -X POST \
+  -H 'Authorization: Bearer $channelAccessToken' \
+  -H 'Content-Length: 0' \
+  https://api.line.me/v2/bot/user/$userId/richmenu/$richmenuId
+EOF;
+  $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+  if(isset($result['message'])) {
+    return $result['message'];
+  }
+  else {
+    return 'success';
+  }
+}
+
+function uploadRandomImageToRichmenu($channelAccessToken, $richmenuId) {
+  if(!isRichmenuIdValid($richmenuId)) {
+    return 'invalid richmenu id';
+  }
+  $randomImageIndex = rand(1, 5);
+  $imagePath = realpath('') . '/' . 'controller_0' . $randomImageIndex . '.png';
+  $sh = <<< EOF
+  curl -X POST \
+  -H 'Authorization: Bearer $channelAccessToken' \
+  -H 'Content-Type: image/png' \
+  -H 'Expect:' \
+  -T $imagePath \
+  https://api.line.me/v2/bot/richmenu/$richmenuId/content
+EOF;
+  $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+  if(isset($result['message'])) {
+    return $result['message'];
+  }
+  else {
+    return 'success. Image #0' . $randomImageIndex . ' has uploaded onto ' . $richmenuId;
+  }
+}
+
+function isRichmenuIdValid($string) {
+  if(preg_match('/^[a-zA-Z0-9-]+$/', $string)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 }
 ?>
